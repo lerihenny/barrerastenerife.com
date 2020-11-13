244a211b9bbcbb79054e463345ced1a818af134e
Jorge Daniel Sosa <jdsosa@gmail.com>
2020-11-13 13:54:37 +0000

    Added dependencies node-sass and google fonts. Created basic scss structure.

--- src/components/layout.js
+++ src/components/layout.js
@@ -11,7 +11,7 @@ import { useStaticQuery, graphql } from "gatsby"
 
 import CustomThemeProvider from "./theme/CustomThemeProvider"
 import Header from "./header"
-import "./layout.css"
+import "../assets/scss/custom-theme.scss"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
