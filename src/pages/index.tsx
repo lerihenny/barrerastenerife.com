dd09a7087c89f23f8cd6a6d4be56d7e8ba057143
Jorge Daniel Sosa <jdsosa@gmail.com>
2021-03-23 19:43:28 +0000

    Add testimonial carousel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,10 +6,10 @@ import Layout from "../components/Layout";
 import SEO from "../components/SEO";
 import MainSlider from "../components/MainSlider";
 import { Banner } from "../components/Banner";
-import { Properties } from "../components/properties/Properties";
 import { ServicesSection } from "../components/ServicesSection";
 import { PropertiesSection } from "../components/PropertiesSection";
 import { useTranslation } from "gatsby-plugin-react-i18next";
+import Slider from "react-slick";
 
 const IndexPage: React.FC<PageProps> = () => {
   const { t } = useTranslation();
@@ -53,7 +53,29 @@ const IndexPage: React.FC<PageProps> = () => {
       </Banner>
 
       <PropertiesSection />
-      <Banner size="medium" image={images.background2.childImageSharp.fluid} />
+      <Banner size="medium" image={images.background2.childImageSharp.fluid}>
+        <Container>
+          <Slider
+            infinite={true}
+            speed={500}
+            slidesToShow={1}
+            slidesToScroll={1}
+            autoplay={true}
+            arrows={false}
+            autoplaySpeed={5000}
+          >
+            <Typography variant="h5" className="testimonial text-center">
+              {`"${t("testimonials.t1")}"`}
+            </Typography>
+            <Typography variant="h5" className="testimonial text-center">
+              {`"${t("testimonials.t2")}"`}
+            </Typography>
+            <Typography variant="h5" className="testimonial text-center">
+              {`"${t("testimonials.t3")}"`}
+            </Typography>
+          </Slider>
+        </Container>
+      </Banner>
     </Layout>
   );
 };
